5bf5984b38c29fdad2a7da26ff8513fb6d09862f
IshaqKhalil <khalilishaq894@gmail.com>
2022-03-01 15:22:08 -0800

    defined parent model, implemented logic for adding parent into mongodb

--- server/models/parentModel.js
+++ server/models/parentModel.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 const parentSchema = mongoose.Schema({
-    
+    studentName: { type: String, required: true },
+    fatherName: { type: String, required: true },
+    email: { type: String, required: true },
+    whatsapp: { type: String, required: true },
+    country: { type: String, required: true },
+    city: { type: String, required: true },
+    password: { type: String, required: true },
+    id: { type: String },
 });
 
 export default mongoose.model("Parents", parentSchema);
