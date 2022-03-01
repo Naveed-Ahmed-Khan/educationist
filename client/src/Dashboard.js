f7f4976d22b7563601ff8b41bfdc30a29fb085d4
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-03-01 18:56:59 +0500

    Merge branch 'Naveed' of https://github.com/Naveed-Ahmed-Khan/educationist

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -4,11 +4,20 @@ import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
 
 const Dashboard = () => {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <>
-      <div className="bg-green-500 md:p-4">
-        <Navbar />
-        <Sidebar />
+      <div className="bg-green-500 z-0 md:p-4">
+        {open ? (
+          <>
+            <Sidebar open={open} setOpen={setOpen} />
+          </>
+        ) : (
+          <>
+            <Navbar open={open} setOpen={setOpen} />
+          </>
+        )}
         <Outlet />
       </div>
     </>
