f6fb95cd16f23e2b81e2a78136e3ccb926b528d7
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-03-04 19:30:18 +0500

    all jobs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#39B54A",
+        secondary: "#262A41",
+        tertiary: "#9D9FA0",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
